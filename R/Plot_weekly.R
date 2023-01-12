09bdd0e9bf11a3cef5d1db43e4797b8bc9289875
Katarina Matthes <katarina.matthes@iem.uzh.ch>
2023-01-12 20:16:22 +0100

    new analysis

---
+++ R/Plot_weekly.R
@@ -0,0 +1,308 @@
+function_plot_weekly <- function() {
+  
+
+dataZH <- read_excel("../data_raw/Master_Zuerich.xlsx", sheet="WeeksMaster2") 
+datapop <-  read_excel("../data_raw/Master_Zuerich.xlsx", sheet="Population") 
+
+
+dataZH_b  <- dataZH %>%
+  mutate(Week= ymd(EndReportingPeriod),
+         iso_cw = isoweek(Week),
+         Year = year(Week),
+         Cw_year= paste0(Year,"/W",iso_cw )) %>%
+  left_join(datapop) %>%
+  mutate(DeathsInc = CityDeathsTotal/CityZurich*1000,
+         InfluenzaInc= CityCases/CityZurich*1000,
+         # InfluenzaInc= ifelse(is.na(InfluenzaInc), 0,InfluenzaInc),
+         HospInc = Total_Aufnahmen/CityZurich*1000,
+         AndereInc = Andere_Infekt/CityZurich*1000,
+         InfluenzaCantonInc = CantonCases/CantonZH*1000,
+         # InfluenzaCantonInc= ifelse(is.na(InfluenzaCantonInc), 0,InfluenzaCantonInc),
+         HospInf = Total_Aufnahmen - Andere_Infekt,
+         HospInfInc =  HospInf/CityZurich*1000 ) 
+
+
+dat.temp <- read.table("../data_raw/order_108443_data.txt",header=TRUE,sep=";")
+
+dat.temp <- dat.temp %>%
+  mutate(datum = ymd(time),
+         Year = year(datum),
+         iso_cw =  isoweek(datum),
+         Cw_year= paste0(Year,"/W",iso_cw )) %>%
+  group_by(Cw_year) %>%
+  summarise(mean_maxium = mean(tre200dx ),
+            mean_minimum = mean(tre200dn),
+            mean_mean = mean(tre200d0))
+
+dataZH_b  <- dataZH_b %>%
+  full_join(dat.temp)
+
+FigureInc <- ggplot() +
+  annotate("rect",xmin=datlim1,xmax=datlim2,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim3,xmax=datlim4,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim5,xmax=datlim6,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim7,xmax=datlim8,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim9,xmax=datlim10,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim11,xmax=datlim12,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim13,xmax=datlim14,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim17,xmax=datlim18,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim19,xmax=datlim20,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim21,xmax=datlim22,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim23,xmax=datlim24,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim25,xmax=datlim26,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim27,xmax=datlim28,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim29,xmax=datlim30,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim31,xmax=datlim32,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim33,xmax=datlim34,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim35,xmax=datlim36,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim37,xmax=datlim38,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim39,xmax=datlim40,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim41,xmax=datlim42,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim43,xmax=datlim44,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim45,xmax=datlim46,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim47,xmax=datlim48,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  geom_line(data=dataZH_b ,aes(y=InfluenzaInc,x= as.POSIXct(Week),colour="City of Zurich"), lwd=lwd_size )+
+  geom_line(data=dataZH_b,aes(y=InfluenzaCantonInc,x=as.POSIXct(Week),colour="Canton Zurich"), lwd=lwd_size ) +
+  scale_x_datetime( breaks = date_breaks("12 month"), 
+                    labels = label_date_short(),
+                    limits =c(min(lims3), max(lims4)),
+                    expand = c(0,0)) +
+  scale_color_manual(name = "",
+                     values = c(col_pal[4],  col_pal[1]))+
+  xlab("Month/Year")+
+  ylab("per 1,000 inhab.")+
+  ggtitle("Influenza cases") +
+  theme_bw()+
+  #theme_light(base_size = 16)+
+  theme(axis.text.y = element_text(size=text_size),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    legend.position = c(.1, .8),
+    legend.text=element_text(size=legend_size),
+    # legend.key.size = unit(1.5, 'cm'),
+    # legend.spacing.x = unit(1.5, 'cm'),
+    axis.text.x = element_blank(),
+    axis.title.x  = element_blank(),
+    axis.title.y  = element_text(size=axis_legend_size),
+    title =element_text(size=title_size))
+         
+
+
+FigureDeath <- ggplot() +
+  # annotate("rect",xmin=datlim15,xmax=datlim16,ymin=-Inf,ymax=Inf,alpha=0.1,fill="orange") +
+  annotate("rect",xmin=datlim1,xmax=datlim2,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim3,xmax=datlim4,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim5,xmax=datlim6,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim7,xmax=datlim8,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim9,xmax=datlim10,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim11,xmax=datlim12,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim13,xmax=datlim14,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim17,xmax=datlim18,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim19,xmax=datlim20,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim21,xmax=datlim22,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim23,xmax=datlim24,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim25,xmax=datlim26,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim27,xmax=datlim28,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim29,xmax=datlim30,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim31,xmax=datlim32,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim33,xmax=datlim34,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim35,xmax=datlim36,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim37,xmax=datlim38,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim39,xmax=datlim40,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim41,xmax=datlim42,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim43,xmax=datlim44,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim45,xmax=datlim46,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim47,xmax=datlim48,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  geom_line(data=dataZH_b ,aes(y=DeathsInc,x= as.POSIXct(Week),col="Death"), lwd=lwd_size )+
+  scale_x_datetime( breaks = date_breaks("12 month"), 
+                    labels = label_date_short(),
+                    limits =c(min(lims3), max(lims4)),
+                    expand = c(0,0)) +
+  scale_color_manual(name = "",
+                   values =col_pal[7])+
+  xlab("Month/Year")+
+  ylab("per 1,000 inhab.")+
+  ggtitle("Number of death") +
+  theme_bw()+
+  #theme_light(base_size = 16)+
+  theme(axis.text.y = element_text(size=text_size),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        legend.position = "none",
+        legend.text=element_text(size=legend_size),
+        # legend.key.size = unit(1.5, 'cm'),
+        # legend.spacing.x = unit(1.5, 'cm'),
+        axis.text.x = element_blank(),
+        axis.title.x  = element_blank(),
+        axis.title.y  = element_text(size=axis_legend_size),
+        title =element_text(size=title_size))
+
+FigureHospInfl <- ggplot() +
+  annotate("rect",xmin=datlim1,xmax=datlim2,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim3,xmax=datlim4,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim5,xmax=datlim6,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim7,xmax=datlim8,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim9,xmax=datlim10,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim11,xmax=datlim12,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim13,xmax=datlim14,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim17,xmax=datlim18,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim19,xmax=datlim20,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim21,xmax=datlim22,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim23,xmax=datlim24,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim25,xmax=datlim26,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim27,xmax=datlim28,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim29,xmax=datlim30,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim31,xmax=datlim32,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim33,xmax=datlim34,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim35,xmax=datlim36,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim37,xmax=datlim38,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim39,xmax=datlim40,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim41,xmax=datlim42,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim43,xmax=datlim44,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim45,xmax=datlim46,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim47,xmax=datlim48,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  # geom_line(data=dataZH_b ,aes(y=HospInfInc,x= as.POSIXct(Week),colour="Total minus other infections"), lwd=lwd_size )+
+  geom_line(data=dataZH_b,aes(y=AndereInc,x=as.POSIXct(Week),colour="Infections incl. influenza"), lwd=lwd_size ) +
+  # geom_line(data=dataZH_b,aes(y= HospInc,x=as.POSIXct(Week),colour="Total"), lwd=lwd_size ) +
+  scale_x_datetime( breaks = date_breaks("12 month"), 
+                    labels = label_date_short(),
+                    limits =c(min(lims3), max(lims4)),
+                    expand = c(0,0)) +
+  scale_color_manual(name = "",
+                     values = c(col_pal[8]))+
+  xlab("Month/Year")+
+  ylab("per 1,000 inhab.")+
+  ggtitle("Hospitalisations - Infections incl. influenza") +
+  theme_bw()+
+  #theme_light(base_size = 16)+
+  theme(axis.text.y = element_text(size=text_size),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        legend.position = "none",
+        legend.text=element_text(size=legend_size),
+        # legend.key.size = unit(1.5, 'cm'),
+        # legend.spacing.x = unit(1.5, 'cm'),
+        axis.text.x = element_blank(),
+        axis.title.x  = element_blank(),
+        axis.title.y  = element_text(size=axis_legend_size),
+        title =element_text(size=title_size))
+    # legend.key.size = unit(2, 'cm'))
+
+FigureHospital <- ggplot() +
+  # annotate("rect",xmin=datlim15,xmax=datlim16,ymin=-Inf,ymax=Inf,alpha=0.1,fill="orange") +
+  annotate("rect",xmin=datlim1,xmax=datlim2,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim3,xmax=datlim4,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim5,xmax=datlim6,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim7,xmax=datlim8,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim9,xmax=datlim10,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim11,xmax=datlim12,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim13,xmax=datlim14,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim17,xmax=datlim18,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim19,xmax=datlim20,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim21,xmax=datlim22,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim23,xmax=datlim24,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim25,xmax=datlim26,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim27,xmax=datlim28,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim29,xmax=datlim30,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim31,xmax=datlim32,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim33,xmax=datlim34,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim35,xmax=datlim36,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim37,xmax=datlim38,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim39,xmax=datlim40,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim41,xmax=datlim42,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim43,xmax=datlim44,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim45,xmax=datlim46,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim47,xmax=datlim48,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  # geom_line(data=dataZH_b ,aes(y=HospInfInc,x= as.POSIXct(Week),colour="Total minus other infections"), lwd=lwd_size )+
+  # geom_line(data=dataZH_b,aes(y=AndereInc,x=as.POSIXct(Week),colour="Infections incl. influenza"), lwd=lwd_size ) +
+  geom_line(data=dataZH_b,aes(y= HospInc,x=as.POSIXct(Week),colour="Total"), lwd=lwd_size ) +
+  scale_x_datetime( breaks = date_breaks("12 month"), 
+                    labels = label_date_short(),
+                    limits =c(min(lims3), max(lims4)),
+                    expand = c(0,0)) +
+  scale_color_manual(name = "",
+                     values = c(col_pal[2]))+
+  xlab("Month/Year")+
+  ylab("per 1,000 inhab.")+
+  ggtitle("Hospitalisations  total") +
+  theme_bw()+
+  #theme_light(base_size = 16)+
+  theme(axis.text.y = element_text(size=text_size),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        legend.position = "none",
+        legend.text=element_text(size=legend_size),
+        # legend.key.size = unit(1.5, 'cm'),
+        # legend.spacing.x = unit(1.5, 'cm'),
+        axis.text.x = element_blank(),
+        axis.title.x  = element_blank(),
+        axis.title.y  = element_text(size=axis_legend_size),
+        title =element_text(size=title_size))
+# legend.key.size = unit(2, 'cm'))
+
+
+FigureTemp <- ggplot() +  
+  # annotate("rect",xmin=datlim15,xmax=datlim16,ymin=-Inf,ymax=Inf,alpha=0.1,fill="orange") +
+  annotate("rect",xmin=datlim1,xmax=datlim2,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim3,xmax=datlim4,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim5,xmax=datlim6,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim7,xmax=datlim8,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim9,xmax=datlim10,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim11,xmax=datlim12,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim13,xmax=datlim14,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim17,xmax=datlim18,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim19,xmax=datlim20,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim21,xmax=datlim22,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim23,xmax=datlim24,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim25,xmax=datlim26,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim27,xmax=datlim28,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim29,xmax=datlim30,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim31,xmax=datlim32,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim33,xmax=datlim34,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim35,xmax=datlim36,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim37,xmax=datlim38,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim39,xmax=datlim40,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim41,xmax=datlim42,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim43,xmax=datlim44,ymin=-Inf,ymax=Inf,alpha=0.4,fill="orange") +
+  annotate("rect",xmin=datlim45,xmax=datlim46,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+  annotate("rect",xmin=datlim47,xmax=datlim48,ymin=-Inf,ymax=Inf,alpha=0.2,fill="grey40") +
+
+  # geom_line(data=dataZH_b  ,aes(y=mean_maxium,x= as.POSIXct(Week),colour="Maximum"), lwd=lwd_size )+
+  geom_line(data=dataZH_b ,aes(y=mean_mean,x=as.POSIXct(Week),colour="Mean"), lwd=lwd_size ) +
+  # geom_line(data=dataZH_b ,aes(y=mean_minimum,x=as.POSIXct(Week),colour="Minimum"), lwd=lwd_size ) +
+  scale_x_datetime( breaks = date_breaks("12 month"), 
+                    labels = label_date_short(),
+                    limits =c(min(lims3), max(lims4)),
+                    expand = c(0,0)) +
+  scale_color_manual(name = "",
+                     values = c(col_pal[3]))+
+  xlab("Month/Year")+
+  ylab("Temperatur in C")+
+  ggtitle("Mean Temperatur") +
+  theme_bw()+
+  #theme_light(base_size = 16)+
+  theme(axis.text.y = element_text(size=text_size),
+        axis.text.x = element_text(size=10,angle=45,hjust=1),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        legend.position = "none",
+        legend.text=element_text(size=legend_size),
+        axis.title.x  = element_blank(),
+        axis.title.y  = element_text(size=axis_legend_size), 
+        title =element_text(size=title_size))
+
+plot_zurich <- cowplot::plot_grid(FigureInc,FigureDeath,FigureHospInfl,FigureHospital, FigureTemp,
+                                  ncol=1, nrow=5, align="hv",
+                                  rel_heights = c(1,1,1,1,1))
+
+
+return(plot_zurich)
+# 
+# plot_zurich <- cowplot::plot_grid(FigureInc,NULL,FigureDeath,NULL,FigureHospital, 
+#                                   ncol=1, nrow=5, align="hv",
+#                                   rel_heights = c(1,-0.1,1,-0.1,1))
+
+
+}
+
+# cowplot::save_plot("output/plot_zurich.pdf", plot_zurich ,base_height=15,base_width=20)
--- master.R
+++ master.R
@@ -46,7 +46,7 @@ conflict_prefer("arrange", "dplyr")
 
 lwd_size <- 1.2
 text_size <- 15
-legend_size <- 16
+legend_size <- 12
 axis_legend_size <- 15
 title_size <- 15
 
@@ -61,6 +61,9 @@ col_pal <- pal_jco()(8)
 
 lims1 <- as.POSIXct(ymd("1909-07-31"))    
 lims2 <- as.POSIXct(ymd("1925-12-31"))    
+
+lims3 <- as.POSIXct(ymd("1910-01-01"))    
+lims4 <- as.POSIXct(ymd("1970-12-31"))    
 
 
 lims_swiss_re1 <- as.POSIXct(ymd("1909-07-31"))    
@@ -88,18 +91,104 @@ datlim12 <- as.POSIXct(ymd("1924-05-10"))
 datlim13 <- as.POSIXct(ymd("1925-01-31"))
 datlim14 <- as.POSIXct(ymd("1925-05-16"))
 
-
 datlim15 <- as.POSIXct(ymd("1914-07-28"))
 datlim16 <- as.POSIXct(ymd("1918-11-11"))
 
+datlim17 <- as.POSIXct(ymd("1926-12-11"))
+datlim18 <- as.POSIXct(ymd("1927-02-19"))
+
+datlim19 <- as.POSIXct(ymd("1928-12-22"))
+datlim20 <- as.POSIXct(ymd("1929-04-20"))
+
+datlim21 <- as.POSIXct(ymd("1932-12-31"))
+datlim22 <- as.POSIXct(ymd("1933-03-18"))
+
+datlim23 <- as.POSIXct(ymd("1957-08-31"))
+datlim24 <- as.POSIXct(ymd("1958-05-17"))
+
+datlim25 <- as.POSIXct(ymd("1932-02-13"))
+datlim26 <- as.POSIXct(ymd("1932-05-07"))
+
+datlim25 <- as.POSIXct(ymd("1932-02-06"))
+datlim26 <- as.POSIXct(ymd("1932-05-07"))
+
+datlim27 <- as.POSIXct(ymd("1939-01-07"))
+datlim28 <- as.POSIXct(ymd("1939-03-25"))
+
+datlim29 <- as.POSIXct(ymd("1911-06-10"))
+datlim30 <- as.POSIXct(ymd("1911-09-09"))
+
+datlim31 <- as.POSIXct(ymd("1947-06-07"))
+datlim32 <- as.POSIXct(ymd("1947-10-04"))
+
+datlim33 <- as.POSIXct(ymd("1952-05-24"))
+datlim34 <- as.POSIXct(ymd("1952-08-23"))
+
+datlim35 <- as.POSIXct(ymd("1938-01-22"))
+datlim36 <- as.POSIXct(ymd("1938-04-23"))
+
+datlim37 <- as.POSIXct(ymd("1943-12-25"))
+datlim38 <- as.POSIXct(ymd("1944-05-13"))
+
+datlim39 <- as.POSIXct(ymd("1950-12-30"))
+datlim40 <- as.POSIXct(ymd("1951-05-12"))
+
+
+datlim41 <- as.POSIXct(ymd("1953-01-10"))
+datlim42 <- as.POSIXct(ymd("1953-05-16"))
+
+datlim43 <- as.POSIXct(ymd("1957-05-25"))
+datlim44 <- as.POSIXct(ymd("1957-08-24"))
+
+datlim45 <- as.POSIXct(ymd("1960-01-02"))
+datlim46 <- as.POSIXct(ymd("1960-04-23"))
+
+
+datlim47 <- as.POSIXct(ymd("1959-01-03"))
+datlim48 <- as.POSIXct(ymd("1959-05-30"))
+
+
+
+# datlim37 <- as.POSIXct(ymd("1940-01-06"))
+# datlim38 <- as.POSIXct(ymd("1940-04-13"))
+# 
+
+# 
+# datlim41 <- as.POSIXct(ymd("1945-01-06"))
+# datlim42 <- as.POSIXct(ymd("1945-06-02"))
+# 
+# datlim43 <- as.POSIXct(ymd("1945-12-22"))
+# datlim44 <- as.POSIXct(ymd("1946-04-20"))
+# 
+# datlim45 <- as.POSIXct(ymd("1946-12-21"))
+# datlim46 <- as.POSIXct(ymd("1947-03-29"))
+# 
+# datlim47 <- as.POSIXct(ymd("1948-12-11"))
+# datlim48 <- as.POSIXct(ymd("1949-04-30"))
+# 
+# datlim49 <- as.POSIXct(ymd("1948-12-11"))
+# datlim50 <- as.POSIXct(ymd("1949-04-30"))
+# 
+# datlim49 <- as.POSIXct(ymd("1948-12-11"))
+# datlim50 <- as.POSIXct(ymd("1949-04-30"))
+# 
+
+
+
+
+
 
 # load scripts
-source("R/Plot_Incidence.R") 
-source("R/Plot_Geburten.R")
-source("R/Plot_Swiss_Re.R")
-source("R/TS_Marriages.R")
-source("R/TS_Birth.R")
-source("R/TS_Marriage_Birth.R")
-source("R/TS_stillborn.R")
+source("R/Plot_weekly.R") 
+# source("R/Plot_Geburten.R")
+# source("R/Plot_Swiss_Re.R")
+# source("R/TS_Marriages.R")
+# source("R/TS_Birth.R")
+# source("R/TS_Marriage_Birth.R")
+# source("R/TS_stillborn.R")
 
-render(paste0("R/City_Zuerich.Rmd"), output_file = paste0("../output/",today(),"_Report_City_Zurich.html"))
+
+render(paste0("R/City_Zuerich_influenza.Rmd"), output_file = paste0("../output/",today(),"_Report_City_Zurich_influenza.html"))
+
+# 
+# render(paste0("R/City_Zuerich.Rmd"), output_file = paste0("../output/",today(),"_Report_City_Zurich.html"))
