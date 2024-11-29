befd5cca559e34f58afd5887dace99530e5e198f
KaMatthes <katarina.matthes@iem.uzh.ch>
2024-11-29 17:46:42 +0100

    minor changes

--- R/RRIR.R
+++ R/RRIR.R
@@ -26,19 +26,19 @@ dt <- dataZH %>%
 ggplot() +
   geom_hline(yintercept=1, col="darkgrey", lwd=0.8)+
   geom_line(data=dt ,aes(y=RIRR ,x= Reporting,col="RIRR (95% CI)"), lwd=lwd_size) +
-  geom_ribbon(data=dt,aes(ymin=LL, ymax=UL,x=Reporting), fill=col_pal[7],linetype=2, alpha=0.3) +
+  geom_ribbon(data=dt,aes(ymin=LL, ymax=UL,x=Reporting), fill=col_pal[8],linetype=2, alpha=0.3) +
   scale_x_date(labels = date_format("%m/%y"), breaks = date_breaks("1 month"),
                limits =c(min(lims5), max(lims6))) +
-  ylim(c(0,10)) +
-
+  scale_y_continuous(breaks=seq(0,10,2),
+                     limits=c(0,10))+
   # geom_vline(data=table_legend, aes(xintercept = as.Date("02.12.1918", "%d.%m.%Y")),col="green", linetype = "dashed", lwd=1) + 
-  annotate("rect",xmin=datlim1,xmax=datlim2,ymin=-Inf,ymax=Inf,alpha=0.1,fill="black") +
-  annotate("rect",xmin=datlim3,xmax=datlim4,ymin=-Inf,ymax=Inf,alpha=0.1,fill="black") +
-  annotate("rect",xmin=datlim5,xmax=datlim6,ymin=-Inf,ymax=Inf,alpha=0.1,fill="black") +
-  annotate("rect",xmin=datlim7,xmax=datlim8,ymin=-Inf,ymax=Inf,alpha=0.1,fill="black") +
+  # annotate("rect",xmin=datlim1,xmax=datlim2,ymin=-Inf,ymax=Inf,alpha=0.1,fill="black") +
+  # annotate("rect",xmin=datlim3,xmax=datlim4,ymin=-Inf,ymax=Inf,alpha=0.1,fill="black") +
+  # annotate("rect",xmin=datlim5,xmax=datlim6,ymin=-Inf,ymax=Inf,alpha=0.1,fill="black") +
+  # annotate("rect",xmin=datlim7,xmax=datlim8,ymin=-Inf,ymax=Inf,alpha=0.1,fill="black") +
   scale_color_manual(name = "",
                      limits =c("RIRR (95% CI)"),
-                     values = c(col_pal[7])) +
+                     values = c(col_pal[8])) +
   xlab("Month/Year")+
   ylab("RIRR")+
   ggtitle("RIRR - Recorded cases") +
@@ -47,11 +47,12 @@ ggplot() +
         panel.grid.major.x = element_blank(),
         panel.grid.minor.x = element_blank(),
         legend.position = c(.62, .82),
-        legend.text=element_text(size=legend_size),
-        axis.text.x = element_blank(),
-        axis.title.x  =element_blank(),
-        axis.title.y  = element_text(size=axis_legend_size),
+        legend.text=element_text(size=25),
+        axis.text.x = element_text(size=18),
+        axis.title.x  =element_text(size=18),
+        axis.title.y  = element_text(size=18),
         title =element_text(size=title_size))
+
 
 
 ggsave("output/RIRR.png" ,h=10,w=25)
